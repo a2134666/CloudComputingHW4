d1692b4d9a5cfcadd01cdc5c6d9f61da8d4d6bf8
a2134666 <a2134666@gmail.com>
2020-11-23 01:37:52 +0800

    code file

---
+++ spark-SGDex.py
@@ -0,0 +1,66 @@
+from pyspark.mllib.classification import LogisticRegressionWithSGD
+from pyspark.mllib.regression import LabeledPoint
+from pyspark import SparkConf, SparkContext
+import numpy as np
+
+def getSparkContext():
+    """
+    Gets the Spark Context
+    """
+    conf = (SparkConf()
+         .setMaster("local") # run on local
+         .setAppName("Logistic Regression") # Name of App
+         .set("spark.executor.memory", "1g")) # Set 1 gig of memory
+    sc = SparkContext(conf = conf) 
+    return sc
+
+sc = getSparkContext()
+
+# Load and parse the data=
+data = sc.textFile("data_banknote_authentication.txt")
+
+def mapper(line):
+    """
+    Mapper that converts an input line to a feature vector
+    """    
+    feats = line.strip().split(",") 
+    # labels must be at the beginning for LRSGD, it's in the end in our data, so 
+    # putting it in the right place
+    label = feats[len(feats) - 1] 
+    feats = feats[: len(feats) - 1]
+    feats.insert(0,label)
+    features = [ float(feature) for feature in feats ] # need floats
+    return np.array(features)
+    #return LabeledPoint(label, feats)
+
+parsedData = data.map(mapper).cache()
+
+def gradient(row, w):
+    Y = row[0]
+    X = row[1:]
+    return ((1.0 / (1.0 + np.exp(-Y * X.dot(w))) - 1.0) * Y * X.T).sum()
+
+def add(x, y):
+    x += y
+    return x
+
+w = 2 * np.random.ranf(size=4) -1
+for i in range(100):
+    #print("Iterarion: {}".format(i+1))
+    w -= parsedData.map(lambda r: gradient(r,w)).reduce(add)
+
+labelsAndPreds = parsedData.map(lambda point: (int(point[0]), 1.0 / (1.0 + np.exp(-1 * w.dot(point[1:])))))
+
+# Train model
+#model = LogisticRegressionWithSGD.train(parsedData)
+
+# Predict the first elem will be actual data and the second
+# item will be the prediction of the model
+#labelsAndPreds = parsedData.map(lambda point: (int(point.label),
+#        model.predict(point.features)))
+
+# Evaluating the model on training data
+trainErr = labelsAndPreds.filter(lambda vp: vp[0] != vp[1]).count() / float(parsedData.count())
+
+# Print some stuff
+print("Training Error = " + str(trainErr))
